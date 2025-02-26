d6ba9f538dde3c7ece07a7889f6c92c42643db5b
huangjun@zz365.com.cn <huangjun@zz365.com.cn>
2025-02-26 10:19:34 +0800

    搭建框架

---
+++ entity-microservices/entity-rt-server/src/main/java/com/zjht/unified/service/IScriptEngine.java
@@ -0,0 +1,13 @@
+package com.zjht.unified.service;
+
+import com.zjht.unified.service.ctx.TaskContext;
+
+/**
+ * var a=ClassUtils.new("ClassA");
+ * var b=a.a1
+ * a.a1="a1 alter"
+ * var b1=InstanceUtils.get(a.guid);
+ */
+public interface IScriptEngine {
+    public void exec(String script, TaskContext ctx);
+}
