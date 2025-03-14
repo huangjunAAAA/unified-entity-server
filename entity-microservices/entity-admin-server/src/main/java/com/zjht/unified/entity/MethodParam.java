d674b4b11761f9d223195df46dea02ddb734406e
liurunjie <liurunjie@zz365.com.cn>
2025-03-14 16:02:18 +0800

    mysqlkeyword

--- entity-microservices/entity-admin-server/src/main/java/com/zjht/unified/entity/MethodParam.java
+++ entity-microservices/entity-admin-server/src/main/java/com/zjht/unified/entity/MethodParam.java
@@ -80,8 +80,8 @@ public class MethodParam extends BaseCopyEntity {
 	 * 参数说明
 	 */
 	@ApiModelProperty(value = "参数说明")
-	@Column(name = "desc")
-	@TableField(value = "desc")  
+	@Column(name = "`desc`")
+	@TableField(value = "`desc`")
 	private String desc;
 
 	 /**
