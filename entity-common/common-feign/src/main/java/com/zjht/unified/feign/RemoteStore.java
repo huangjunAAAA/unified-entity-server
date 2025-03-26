f320e3fdc04ae37d8f50cde5ca490c4b2bfd3550
liurunjie <liurunjie@zz365.com.cn>
2025-03-26 11:43:25 +0800

    add @RequestParam

--- entity-common/common-feign/src/main/java/com/zjht/unified/feign/RemoteStore.java
+++ entity-common/common-feign/src/main/java/com/zjht/unified/feign/RemoteStore.java
@@ -3,6 +3,7 @@ package com.zjht.unified.feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 import java.util.Map;
@@ -10,5 +11,5 @@ import java.util.Map;
 @FeignClient(value = "entity-store-server", configuration = IgnoreValidateFormDataConfiguration.class)
 public interface RemoteStore {
     @PostMapping("/rt/query")
-    List<Map<String,Object>> query(String ver,String prjId,String sql);
+    List<Map<String,Object>> query(@RequestParam String ver, @RequestParam String prjId,  @RequestParam String sql);
 }
--- entity-microservices/entity-store-server/src/main/java/com/zjht/unified/data/api/controller/QueryController.java
+++ entity-microservices/entity-store-server/src/main/java/com/zjht/unified/data/api/controller/QueryController.java
@@ -3,6 +3,7 @@ package com.zjht.unified.data.api.controller;
 
 import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -12,7 +13,7 @@ import java.util.Map;
 @Api(value = "查询控制器",tags = {"查询控制器"})
 public class QueryController {
     @PostMapping("/rt/query")
-    List<Map<String, Object>> query(String ver, String prjId, String sql) {
+    List<Map<String, Object>> query(@RequestParam String ver, @RequestParam String prjId, @RequestParam String sql) {
         //todo
         return null;
     }
