f0bbbdd15854a73a44a978249a425c8a2a983bb5
huangjun@zz365.com.cn <huangjun@zz365.com.cn>
2025-03-28 18:04:16 +0800

    实现基于模型的增删改查

--- entity-microservices/ui-server/src/main/java/com/zjht/ui/controller/PageModelController.java
+++ entity-microservices/ui-server/src/main/java/com/zjht/ui/controller/PageModelController.java
@@ -2,14 +2,23 @@ package com.zjht.ui.controller;
 
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.wukong.core.mp.base.BaseEntity;
 import com.zjht.ui.dto.ModelPrjDTO;
 import com.zjht.ui.entity.UiPage;
 import com.zjht.ui.entity.UiPrj;
 import com.zjht.ui.service.IUiPageService;
 import com.zjht.ui.service.IUiPrjService;
 import com.zjht.ui.service.PageModelService;
+import com.zjht.ui.vo.UiPageVo;
+import com.zjht.ui.wrapper.UiPageWrapper;
 import com.zjht.unified.common.core.constants.Constants;
+import com.zjht.unified.common.core.domain.PageDomain;
 import com.zjht.unified.common.core.domain.R;
+import com.zjht.unified.common.core.domain.TableDataInfo;
+import com.zjht.unified.common.core.domain.dto.BaseQueryDTO;
 import com.zjht.unified.domain.composite.PrjSpecDO;
 import com.zjht.unified.domain.exchange.CID;
 import com.zjht.unified.domain.exchange.PageSpec;
@@ -72,4 +81,31 @@ public class PageModelController {
     }
 
 
+    /**
+     * 查询页面表列表, 对象形式
+     */
+    @ApiOperation(value = "查询页面表列表")
+    @PostMapping("/list-ext")
+    public TableDataInfo<PageSpec> listExt(@RequestBody BaseQueryDTO<UiPage> uiPage)
+    {
+        return list(uiPage.getCondition(),new PageDomain(uiPage.getPage(),uiPage.getSize()));
+    }
+
+    /**
+     * 查询页面表列表
+     */
+    @ApiOperation(value = "查询页面表列表")
+    @GetMapping("/list")
+    public TableDataInfo<PageSpec> list(UiPage uiPage, PageDomain  pageDomain)
+    {
+        TableDataInfo<PageSpec> dataInfo = new TableDataInfo();
+        Page<UiPage> page = new Page<>(pageDomain.getPageNum(),pageDomain.getPageSize());
+        IPage<UiPage> pageList = uiPageService.page(page, Wrappers.<UiPage>lambdaQuery(uiPage).orderByDesc(BaseEntity::getCreateTime));
+        List<PageSpec> pageSpecsList = pageList.getRecords().stream().map(p->pageModelService.getPage(p.getId(),p.getGuid())).collect(Collectors.toList());
+        dataInfo.setCode(Constants.SUCCESS);
+        dataInfo.setData(pageSpecsList);
+        dataInfo.setMsg("查询成功");
+        dataInfo.setTotal(pageList.getTotal());
+        return dataInfo;
+    }
 }
