31eb5b9a9a3ebad435a5c3f7bc2bb320aa29cba3
liurunjie <liurunjie@zz365.com.cn>
2025-03-31 18:20:36 +0800

    test

--- entity-common/google-v8/src/test/java/V8Test.java
+++ entity-common/google-v8/src/test/java/V8Test.java
@@ -1,3 +1,4 @@
+import com.caoccao.javet.annotations.V8Function;
 import com.caoccao.javet.interception.logging.JavetStandardConsoleInterceptor;
 import com.caoccao.javet.interop.V8Runtime;
 import com.caoccao.javet.interop.converters.JavetProxyConverter;
@@ -6,6 +7,7 @@ import com.caoccao.javet.interop.engine.IJavetEnginePool;
 import com.caoccao.javet.interop.engine.JavetEnginePool;
 import com.caoccao.javet.interop.proxy.JavetDirectProxyFunctionHandler;
 import com.caoccao.javet.values.V8Value;
+import com.caoccao.javet.values.reference.V8ValueFunction;
 import com.caoccao.javet.values.reference.V8ValueObject;
 
 import lombok.extern.slf4j.Slf4j;
@@ -20,7 +22,8 @@ public class V8Test {
     private static final IJavetEnginePool<V8Runtime> javetEnginePool = new JavetEnginePool<>();
 
     public static void test2() {
-        Object o = exec(" tst.m1(1,2,'3')");
+//        Object o = exec(" tst.m1('tst',1,2,'3'); ");
+        Object o = exec(" var a = V8Test.new(); a.name = 123;  a.m2(1); ");
         System.out.println("o = " + o);
 
     }
@@ -28,6 +31,21 @@ public class V8Test {
 
     public static void main(String[] args) {
         test2();
+    }
+
+    @V8Function(name = "new")
+    public V8Value newInstance( V8Value... args) throws Exception {
+        V8Value v8Value = new JavetProxyConverter().toV8Value(getRuntime(), new TestProxyObject());
+        if (v8Value instanceof V8ValueObject) {
+            V8ValueObject value = (V8ValueObject) v8Value;
+            V8ValueFunction m2Function = (V8ValueFunction) getRuntime()
+                    .getExecutor("(function() { return function(x) { return this.name + x; }; })()")
+                    .execute();
+
+            value.set("m2", m2Function);
+
+        }
+        return v8Value;
     }
 
 
@@ -49,7 +67,12 @@ public class V8Test {
             V8Runtime v8Runtime = javetEngine.getV8Runtime();
             JavetStandardConsoleInterceptor consoleInterceptor = new JavetStandardConsoleInterceptor(v8Runtime);
             consoleInterceptor.register(v8Runtime.getGlobalObject());
-            return v8Runtime;
+
+            try (V8ValueObject v8ValueObject = v8Runtime.createV8ValueObject()) {
+                v8Runtime.getGlobalObject().set("V8Test", v8ValueObject);
+                V8Test v8Test = new V8Test();
+                v8ValueObject.bind(v8Test);
+            }            return v8Runtime;
         } catch (Exception e) {
             log.error(e.getMessage(), e);
         }
