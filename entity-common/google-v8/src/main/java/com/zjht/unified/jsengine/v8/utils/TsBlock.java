2f382e61e52f427ea58bb0a647ef5bdfa7b4fa4b
huangjun@zz365.com.cn <huangjun@zz365.com.cn>
2025-07-17 17:45:41 +0800

    测试各种表达式2

--- entity-common/google-v8/src/main/java/com/zjht/unified/jsengine/v8/utils/TsBlock.java
+++ entity-common/google-v8/src/main/java/com/zjht/unified/jsengine/v8/utils/TsBlock.java
@@ -1,3 +1,5 @@
+package com.zjht.unified.jsengine.v8.utils;
+
 import lombok.Data;
 
 import java.util.ArrayList;
@@ -8,5 +10,4 @@ public class TsBlock {
     private List<String> declVars=new ArrayList<>();
     private String body;
     private List<String> deps=new ArrayList<>();
-    private boolean use;
 }
